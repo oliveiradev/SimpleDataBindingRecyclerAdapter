0c237898ad603a5aac54f3aa9081cd2bbb08cbbf
Felipe Oliveira <felipe.oliveiradev@gmail.com>
2017-10-21 23:29:01 -0300

    Use just children of view data binding on simple view holder

--- lib/src/main/java/oliveiradev/com/github/lib/SimpleBaseViewHolder.java
+++ lib/src/main/java/oliveiradev/com/github/lib/SimpleBaseViewHolder.java
@@ -1,10 +1,12 @@
 package oliveiradev.com.github.lib;
 
+import android.databinding.ViewDataBinding;
+
 /**
  * Created by felipe on 30/05/17.
  */
 
-public interface SimpleBaseViewHolder<T, R> {
+public interface SimpleBaseViewHolder<T extends ViewDataBinding, R> {
 
     void getBinding(T binding);
 
